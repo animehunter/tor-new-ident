638225a88bc276dfc71799ceee6f1e3922e0db46
root <root@huntercool.(none)>
2013-07-18 22:26:12 +0200

    add check for None

--- newtorident.py
+++ newtorident.py
@@ -3,5 +3,5 @@ from stem import Signal
 
 con = stem.connection.connect_socket_file('/var/run/tro/control')
 if con is None: con = stem.connection.connect_socket_file('/var/run/tor/control')
-con.signal(Signal.NEWNYM)
+if con: con.signal(Signal.NEWNYM)
 
